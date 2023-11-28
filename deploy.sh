1ecb6e7a4d8fa3cec0573cbdbfccd76af9b5c503
Mr <404792402@qq.com>
2023-11-28 17:49:40 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/whenTheMorningDark/element-learn.git main:gh-pages
+git push -f https://github.com/whenTheMorningDark/element-learn.git master:gh-pages
 
 cd -
